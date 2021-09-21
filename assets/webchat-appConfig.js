f2193e89675f9176fbcb26021c8b62e3dc119d11
akondratsky <akondratsky@yandex.ru>
2021-09-21 15:57:57 +0300

    hosted

---
+++ assets/webchat-appConfig.js
@@ -0,0 +1,9 @@
+var appConfig = {
+    //  change the your AccountSid
+    accountSid: "AC906e4ba153778300e42a455d5813fcfa",
+    // change to your Flex Flow SID
+    flexFlowSid: "FOfc6f48ddefc75a771a3832ca42cf0997",
+    colorTheme: {
+        // overrides: brandedColors
+    }
+}
---
+++ precache-manifest.9041ad8ce5ac460a7bdee2a8a58b338a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ab085e0c28c8933ec9d1d68621f9b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c8fe54c26d37e52685c",
+    "url": "/static/css/main.aed80c0b.chunk.css"
+  },
+  {
+    "revision": "39065b5322569eab84d9",
+    "url": "/static/js/2.6da17fe8.chunk.js"
+  },
+  {
+    "revision": "878f959acf21c1c89e8af12b216b7978",
+    "url": "/static/js/2.6da17fe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c8fe54c26d37e52685c",
+    "url": "/static/js/main.5ac07808.chunk.js"
+  },
+  {
+    "revision": "86389fed35a3bb5a420d",
+    "url": "/static/js/runtime-main.274e4769.js"
+  }
+]);
